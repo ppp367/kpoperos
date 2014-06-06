d93756ce40943bf5e7e1e262ff488e2a06b15841
Pedro Mota <ppp367@hotmail.com>
2014-06-06 15:49:41 -0700

    agregado controlador Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Kpoperos::Application.routes.draw do
  
+  get "dashboard/home"
   get "home", to: 'static_pages#home'
   get "about", to: 'static_pages#about'
   get "contest", to: 'static_pages#contest'
