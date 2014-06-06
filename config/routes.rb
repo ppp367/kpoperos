35ce24fe01787f71f487bdd8b62fa4f8fda80bd2
Pedro Mota <ppp367@hotmail.com>
2014-06-05 23:46:11 -0700

    controlador StaticPages Agregado con secciones home conferencias concurso

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Kpoperos::Application.routes.draw do
+  get "static_pages/concurso"
+  get "static_pages/home"
+  get "static_pages/conferencias"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
