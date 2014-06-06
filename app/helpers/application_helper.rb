17b087646a209136d6d4e1c766ab8e6071c0f1b3
Pedro Mota <ppp367@hotmail.com>
2014-06-06 00:42:36 -0700

    Agregado Helper que inserta Tiulo entero dependiendo de cada pagina

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#Inserta el titulo completo a la pagina
+	def full_title(page_title)
+		base_title = "Kpoperos"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
