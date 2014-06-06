71aa9c1a1f1137d6d943710c47e34e8da130f2ca
Pedro Mota <ppp367@hotmail.com>
2014-06-06 16:16:23 -0700

    Agregada columna ROLES a USERS para especificar el tipo de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606132452) do
+ActiveRecord::Schema.define(version: 20140606231507) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140606132452) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "role"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
