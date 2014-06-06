fda2735ce7815898cf05e06f1117ca4383c7cf99
Pedro Mota <ppp367@hotmail.com>
2014-06-06 13:53:05 -0700

    Nombre de pagina estática cambiado a inglés para pruebas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def conferencias
+  def info
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Kpoperos::Application.routes.draw do
   devise_for :users
   get "concurso", to: 'static_pages#concurso'
-  get "conferencias", to: 'static_pages#conferencias'
+  get "info", to: 'static_pages#info'
   get "home", to: 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
