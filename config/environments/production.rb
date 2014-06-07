933bba55a755734000654781d7c57304462644a2
Pedro Mota <ppp367@hotmail.com>
2014-06-06 19:30:17 -0700

    Idiomas de Active record agregados a entorno de produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Kpoperos::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.i18n.available_locales = :es , :en , :ko
 end
