adf1e8eb4474322f0f0512a467ba4a4f1f83c161
Pedro Mota <ppp367@hotmail.com>
2014-06-10 23:47:37 -0700

    host cambiado de lguar

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Kpoperos::Application.initialize!
-
-config.action_mailer.default_url_options = { :host => 'kpoperos.herokuapp.com' }
