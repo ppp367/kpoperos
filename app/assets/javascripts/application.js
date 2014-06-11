1a37367c213215296738f3199203313ad6ecaca3
Pedro Mota <ppp367@hotmail.com>
2014-06-10 20:04:20 -0700

    junk removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,5 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require bootstrap
-//= require bootstrap-dropdown
-//= require bootbox
 //= require turbolinks
-//= require_tree .
 //= require_tree
