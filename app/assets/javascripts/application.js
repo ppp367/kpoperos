38b56c3c604fbf3643c98574aa69eb8ce5d280b9
Pedro Mota <ppp367@hotmail.com>
2014-06-13 22:04:32 -0700

    js modified

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,14 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require twitter/bootstrap
 //= require jquery
-//= require_tree
+#= require twitter/bootstrap/bootstrap-modal
+#= require twitter/bootstrap/bootstrap-dropdown
+#= require twitter/bootstrap/bootstrap-transition
+#= require twitter/bootstrap/bootstrap-alert
+#= require twitter/bootstrap/bootstrap-button
+#= require twitter/bootstrap/bootstrap-collapse
+//= require_tree .
 
 $(function(){
   $('.dropdown-toggle').dropdown()
