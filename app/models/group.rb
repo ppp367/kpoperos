d79dad08c7e9001f1ab387c14c8f3050984eee55
Pedro Mota <ppp367@hotmail.com>
2014-06-15 14:50:27 -0700

    grupo set commentable

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base  
   acts_as_group :members => [:users], :default_members => :users
   acts_as_votable
+  acts_as_commentable
   validates :name, :presence => true, uniqueness: true
   
 
