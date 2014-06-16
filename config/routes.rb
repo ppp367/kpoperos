546baaefbf43825dab2383d4bb18322720d38b80
Pedro Mota <ppp367@hotmail.com>
2014-06-15 20:48:17 -0700

    empieza modificacion perfiles

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,6 @@ Kpoperos::Application.routes.draw do
   end
 
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
