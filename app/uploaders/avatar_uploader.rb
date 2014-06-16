8c4326bd4743c9c490b57d18e0a628f940257409
Pedro Mota <ppp367@hotmail.com>
2014-06-16 02:02:44 -0700

    uploader funcionando - no rescalable

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  #include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -31,21 +31,21 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
 
   # Process files as they are uploaded:
-  process :scale => [200, 200]
+  #process :scale => [200, 200]
   #
   def scale(width, height)
   #  do something
   end
 
-  process :convert => 'png'
+  #process :convert => 'png'
 
   def filename
     super.chomp(File.extname(super)) + '.png' if original_filename.present?
   end
   # Create different versions of your uploaded files:
-   version :thumb do
-     process :resize_to_fit => [100, 100]
-   end
+   #version :thumb do
+    #process :resize_to_fit => [100, 100]
+   #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
